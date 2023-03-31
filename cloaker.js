7600bc450af55070ca6201056b0783301ae30a01
LiamSirBean <122816237+LiamSirBean@users.noreply.github.com>
2023-03-31 16:29:13 -0400

    Update cloaker.js

--- cloaker.js
+++ cloaker.js
@@ -28,36 +28,4 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
 
         doc.body.appendChild(iframe)
         location.replace("https://google.com")
-    }
-}
-
-function clockTime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
-    
-    if(h == 0){
-        h = 12;
-    }
-    
-    if(h > 12){
-        h = h - 12;
-        session = "PM";
-    }
-    
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
-    
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClock").innerText = time;
-    document.getElementById("MyClock").textContent = time;
-    
-    setTimeout(clockTime, 1000);
-    
-};
-
-
-
+    };
