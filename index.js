2f2d412856606e4ee789f706061c702519d6f33b
LiamSirBean <122816237+LiamSirBean@users.noreply.github.com>
2023-04-04 20:40:50 +0000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// Get the button element by its ID
-const continueButton = document.getElementById('continue-button');
-
-// Add a click event listener to the button
-continueButton.addEventListener('click', () => {
-  // Redirect the user to the "about.html" page in the "pages" directory
-  window.location.href = 'index.html';
-});
