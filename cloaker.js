52bc5c4b335454fb66d34dcf866a82aceeae30cb
LiamSirBean <122816237+LiamSirBean@users.noreply.github.com>
2023-04-09 15:24:46 -0400

    Update cloaker.js

--- cloaker.js
+++ cloaker.js
@@ -31,6 +31,6 @@ if (!inFrame && !navigator.userAgent.includes("Firefox")) {
 
         doc.head.appendChild(link);
         doc.body.appendChild(iframe)
-        location.replace("https://google.com")
+        location.replace("https://example.com")
     }
 }
