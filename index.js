d1cd875fda36eccb8df6dc2063a6d80089f31ee8
LiamSirBean <rfwrl13@gmail.com>
2023-04-15 17:34:26 -0400

    j

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
-addEventListener("fetch", (event) => {
-    event.respondWith(handleRequest(event.request));
+const uvConfig = JSON.parse(Buffer.from(process.env.UV_CONFIG, 'base64').toString());
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  // Set the target URL for the proxy
+  const targetUrl = uvConfig.targetUrl;
+
+  // Create the request options object
+  const options = {
+    method: request.method,
+    headers: request.headers,
+  };
+
+  // Make the proxy request
+  const proxyRes = await fetch(targetUrl + request.url, options);
+
+  // Set the response headers
+  const headers = new Headers(proxyRes.headers);
+  headers.delete('content-security-policy');
+  headers.delete('content-security-policy-report-only');
+  headers.delete('clear-site-data');
+
+  // Relay the response from the target server back to the client
+  return new Response(proxyRes.body, {
+    status: proxyRes.status,
+    statusText: proxyRes.statusText,
+    headers,
   });
-  
-  async function handleRequest(request) {
-    const url = new URL(request.url);
-    const targetUrl = "https://" + url.searchParams.get("url");
-  
-    const options = {
-      method: request.method,
-      headers: request.headers,
-    };
-  
-    const proxyRes = await fetch(targetUrl, options);
-  
-    const headers = new Headers(proxyRes.headers);
-    headers.delete("content-security-policy");
-    headers.delete("content-security-policy-report-only");
-    headers.delete("clear-site-data");
-  
-    const encodedUrl = encodeURIComponent(targetUrl);
-  
-    return new Response(proxyRes.body, {
-      status: proxyRes.status,
-      statusText: proxyRes.statusText,
-      headers: {
-        ...headers,
-        "Access-Control-Allow-Origin": "*",
-        "Content-Security-Policy": "default-src 'none';",
-        "Referrer-Policy": "no-referrer",
-      },
-    });
-  }
-  
+}
